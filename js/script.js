26b91eecabaf89a6e008edb5fd554ff2b98a3e9e
michifehr <mifehr@bluewin.ch>
2026-06-10 14:54:22 +0200

    remove style switcher and make form work

--- js/script.js
+++ js/script.js
@@ -165,3 +165,40 @@ window.onclick = function (event) {
         closePDF();
     }
 };
+
+// =========================
+// CONTACT FORM
+// =========================
+  const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    name: form.querySelector('input[name="name"]').value,
+    email: form.querySelector('input[name="email"]').value,
+    subject: form.querySelector('input[name="subject"]').value,
+    message: form.querySelector('textarea[name="message"]').value
+  };
+
+  try {
+    const response = await fetch("https://formspree.io/f/xaqzbaqj", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+      alert("Nachricht erfolgreich gesendet!");
+      form.reset();
+    } else {
+      alert("Fehler beim Senden der Nachricht.");
+    }
+  } catch (error) {
+    alert("Netzwerkfehler.");
+    console.error(error);
+  }
+});
